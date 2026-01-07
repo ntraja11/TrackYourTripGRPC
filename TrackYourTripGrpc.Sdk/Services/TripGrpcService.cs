2613e6bd57dc6911f2f2a37c3ee69ab75a369cd2
Raja Anand <ntraja11@gmail.com>
2026-01-07 10:32:20 +0100

    Grpc wrapper created for Tripservice

--- TrackYourTripGrpc.Sdk/Services/TripGrpcService.cs
+++ TrackYourTripGrpc.Sdk/Services/TripGrpcService.cs
@@ -1,4 +1,5 @@
 ﻿using Grpc.Core;
+using System.Diagnostics;
 using TrackYourTripGrpc.Sdk.Interfaces;
 using TrackYourTripGRPCApi.Protos;
 
@@ -21,14 +22,112 @@ public class TripGrpcService : ITripGrpcService
         }
         catch (RpcException ex)
         {
-            System.Diagnostics.Debug.WriteLine($"gRPC ERROR: {ex.StatusCode} - {ex.Status.Detail}");
+            Debug.WriteLine($"gRPC ERROR: {ex.StatusCode} - {ex.Status.Detail}");
             throw;
         }
         catch (Exception ex)
         {
-            System.Diagnostics.Debug.WriteLine($"UNEXPECTED ERROR: {ex.Message}");
+            Debug.WriteLine($"UNEXPECTED ERROR: {ex.Message}");
             throw;
         }
+    }
 
+    public async Task<IEnumerable<TripDetail>> GetAllTripsAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var request = new Google.Protobuf.WellKnownTypes.Empty();
+            var response = await _tripClient.GetAllTripsAsync(request, cancellationToken: cancellationToken);
+            return response.Trips;
+        }
+        catch (RpcException ex)
+        {
+            Debug.WriteLine($"gRPC ERROR: {ex.StatusCode} - {ex.Status.Detail}");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"UNEXPECTED ERROR: {ex.Message}");
+            throw;
+        }
+    }
+
+    public async Task<TripDetail> CreateTripAsync(TripDetail tripDetail, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var request = new CreateTripRequest { 
+                Title = tripDetail.Title, 
+                Description = tripDetail.Description,
+                CreatedByUserEmail = tripDetail.CreatedByUserEmail,
+                StartDate = tripDetail.StartDate,
+                From = tripDetail.From,
+                To = tripDetail.To,
+                Notes = tripDetail.Notes,
+            };
+            var response = await _tripClient.CreateTripAsync(request, cancellationToken: cancellationToken);
+            return response.Trip;
+        }
+        catch (RpcException ex)
+        {
+            Debug.WriteLine($"gRPC ERROR: {ex.StatusCode} - {ex.Status.Detail}");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"UNEXPECTED ERROR: {ex.Message}");
+            throw;
+        }
+    }
+
+    public async Task<TripDetail> UpdateTripAsync(TripDetail tripDetail, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var request = new UpdateTripRequest
+            {
+                Id = tripDetail.Id,
+                Title = tripDetail.Title,
+                Description = tripDetail.Description,
+                StartDate = tripDetail.StartDate,
+                From = tripDetail.From,
+                To = tripDetail.To,
+                Notes = tripDetail.Notes,
+                EndDate = tripDetail.EndDate,
+                Status = tripDetail.Status
+            };
+            var response = await _tripClient.UpdateTripAsync(request, cancellationToken: cancellationToken);
+            return response.Trip;
+        }
+        catch (RpcException ex)
+        {
+            Debug.WriteLine($"gRPC ERROR: {ex.StatusCode} - {ex.Status.Detail}");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"UNEXPECTED ERROR: {ex.Message}");
+            throw;
+        }
+    }
+
+    public async Task<bool> DeleteTripAsync(int tripId, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var request = new DeleteTripRequest { Id = tripId };
+            var response = await _tripClient.DeleteTripAsync(request, cancellationToken: cancellationToken);
+            return response.Success;
+        }
+        catch (RpcException ex)
+        {
+            Debug.WriteLine($"gRPC ERROR: {ex.StatusCode} - {ex.Status.Detail}");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"UNEXPECTED ERROR: {ex.Message}");
+            throw;
+        }
     }
 }
